cd2418550c68fdec12b9e5ce428a69b57bc7a961
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-02-04 23:26:44 -0700

    chore: Add codegen files

---
+++ apps/flame-defi/app/earn/gql/gql.ts
@@ -0,0 +1,46 @@
+/* eslint-disable */
+import * as types from './graphql';
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+
+/**
+ * Map of all GraphQL operations in the project.
+ *
+ * This map has several performance disadvantages:
+ * 1. It is not tree-shakeable, so it will include all operations in the project.
+ * 2. It is not minifiable, so the string of a GraphQL query will be multiple times inside the bundle.
+ * 3. It does not support dead code elimination, so it will add unused operations.
+ *
+ * Therefore it is highly recommended to use the babel or swc plugin for production.
+ * Learn more about it here: https://the-guild.dev/graphql/codegen/plugins/presets/preset-client#reducing-bundle-size
+ */
+type Documents = {
+    "\n  query Vaults {\n    vaults {\n      items {\n        address\n        symbol\n        name\n        creationBlockNumber\n        creationTimestamp\n        creatorAddress\n        whitelisted\n        asset {\n          id\n          address\n          decimals\n        }\n        chain {\n          id\n          network\n        }\n        state {\n          id\n          apy\n          netApy\n          totalAssets\n          totalAssetsUsd\n          fee\n          timelock\n        }\n      }\n    }\n  }\n": typeof types.VaultsDocument,
+};
+const documents: Documents = {
+    "\n  query Vaults {\n    vaults {\n      items {\n        address\n        symbol\n        name\n        creationBlockNumber\n        creationTimestamp\n        creatorAddress\n        whitelisted\n        asset {\n          id\n          address\n          decimals\n        }\n        chain {\n          id\n          network\n        }\n        state {\n          id\n          apy\n          netApy\n          totalAssets\n          totalAssetsUsd\n          fee\n          timelock\n        }\n      }\n    }\n  }\n": types.VaultsDocument,
+};
+
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ *
+ *
+ * @example
+ * ```ts
+ * const query = graphql(`query GetUser($id: ID!) { user(id: $id) { name } }`);
+ * ```
+ *
+ * The query argument is unknown!
+ * Please regenerate the types.
+ */
+export function graphql(source: string): unknown;
+
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n  query Vaults {\n    vaults {\n      items {\n        address\n        symbol\n        name\n        creationBlockNumber\n        creationTimestamp\n        creatorAddress\n        whitelisted\n        asset {\n          id\n          address\n          decimals\n        }\n        chain {\n          id\n          network\n        }\n        state {\n          id\n          apy\n          netApy\n          totalAssets\n          totalAssetsUsd\n          fee\n          timelock\n        }\n      }\n    }\n  }\n"): (typeof documents)["\n  query Vaults {\n    vaults {\n      items {\n        address\n        symbol\n        name\n        creationBlockNumber\n        creationTimestamp\n        creatorAddress\n        whitelisted\n        asset {\n          id\n          address\n          decimals\n        }\n        chain {\n          id\n          network\n        }\n        state {\n          id\n          apy\n          netApy\n          totalAssets\n          totalAssetsUsd\n          fee\n          timelock\n        }\n      }\n    }\n  }\n"];
+
+export function graphql(source: string) {
+  return (documents as any)[source] ?? {};
+}
+
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
