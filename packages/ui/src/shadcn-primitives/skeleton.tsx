c21d4e1c40936ba572945755ef0c0c9a0c83d2de
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-02-05 19:11:42 -0700

    feat: `Skeleton` component

---
+++ packages/ui/src/shadcn-primitives/skeleton.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+
+import { cn } from "../lib/utils";
+
+interface SkeletonProps extends React.PropsWithChildren {
+  className?: string;
+  isLoading?: boolean;
+}
+
+const Skeleton = React.forwardRef<HTMLDivElement, SkeletonProps>(
+  ({ children, className, isLoading }, forwardedRef) => {
+    if (!isLoading) {
+      return children;
+    }
+
+    return (
+      <div
+        ref={forwardedRef}
+        aria-hidden
+        className={cn(
+          "animate-pulse rounded-xs bg-primary/10 *:invisible",
+          className
+        )}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+Skeleton.displayName = "Skeleton";
+
+export { Skeleton };
+export type { SkeletonProps };
