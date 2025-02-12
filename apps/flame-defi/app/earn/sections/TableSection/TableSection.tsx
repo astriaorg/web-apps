0779a397a576a68d9f57003f6324301967bd362d
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-02-11 17:43:02 -0700

    refactor: Rename `TableCard` to `Card`

--- apps/flame-defi/app/earn/sections/TableSection/TableSection.tsx
+++ apps/flame-defi/app/earn/sections/TableSection/TableSection.tsx
@@ -1,9 +1,5 @@
-import {
-  Table,
-  TableCard,
-  TablePagination,
-  TableSearch,
-} from "earn/components/Table";
+import { Card } from "earn/components/Card";
+import { Table, TablePagination, TableSearch } from "earn/components/Table";
 import { useTable } from "earn/hooks/useTable";
 
 export const TableSection = () => {
@@ -11,25 +7,25 @@ export const TableSection = () => {
 
   return (
     <section className="flex flex-col p-4 md:p-20">
-      <div className="flex justify-end w-full mb-6">
+      <div className="flex w-full mb-4">
         <TableSearch />
       </div>
 
       {status === "error" && (
-        <TableCard className="h-[250px] text-lg text-grey-light flex items-center justify-center">
+        <Card className="h-[250px] text-lg text-grey-light flex items-center justify-center">
           {`We couldn't fetch vault data. Please try again later.`}
-        </TableCard>
+        </Card>
       )}
       {status === "empty" && (
-        <TableCard className="h-[250px] text-lg text-grey-light flex items-center justify-center">
+        <Card className="h-[250px] text-lg text-grey-light flex items-center justify-center">
           {`No vaults found.`}
-        </TableCard>
+        </Card>
       )}
       {status === "success" && (
         <>
-          <TableCard>
+          <Card className="overflow-x-hidden md:overflow-x-auto">
             <Table />
-          </TableCard>
+          </Card>
 
           <div className="flex justify-center mt-10">
             <TablePagination />
