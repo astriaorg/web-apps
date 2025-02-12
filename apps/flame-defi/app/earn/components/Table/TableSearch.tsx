0324a88f13af735248f2b9e2d956661c48b76526
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-02-11 17:44:36 -0700

    feat: Animation on search input

--- apps/flame-defi/app/earn/components/Table/TableSearch.tsx
+++ apps/flame-defi/app/earn/components/Table/TableSearch.tsx
@@ -1,8 +1,14 @@
 import { SearchIcon } from "@repo/ui/icons";
+import { cn } from "@repo/ui/lib";
 import { Input, Skeleton } from "@repo/ui/shadcn-primitives";
 import { useTable } from "earn/hooks/useTable";
+import { useRef, useState } from "react";
 
 export const TableSearch = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [isFocused, setIsFocused] = useState(false);
+
   const {
     search,
     setSearch,
@@ -15,12 +21,33 @@ export const TableSearch = () => {
       className="w-[200px]"
     >
       <Input
+        ref={inputRef}
         value={search}
         onChange={(e) => setSearch(e.target.value)}
+        onBlur={() => setIsFocused(false)}
+        onFocus={() => setIsFocused(true)}
         type="text"
-        placeholder="Search vaults"
-        startAdornment={<SearchIcon size={16} />}
-        className="w-[200px]"
+        endAdornment={
+          <div
+            className={cn(
+              "transition-all duration-300 ease-in-out",
+              !search && !isFocused && "-ml-10",
+            )}
+            onClick={() => {
+              if (inputRef.current && !isFocused) {
+                setIsFocused(true);
+                inputRef.current.focus();
+              }
+            }}
+          >
+            <SearchIcon aria-label="Search" size={24} />
+          </div>
+        }
+        className={cn(
+          "transition-all duration-300 ease-in-out",
+          search ? "w-[200px]" : "w-0 border-transparent outline-none",
+          "focus-visible:w-[200px] focus-visible:border-orange-soft",
+        )}
       />
     </Skeleton>
   );
