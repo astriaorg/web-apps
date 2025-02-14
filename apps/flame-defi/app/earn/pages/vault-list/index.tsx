918b1c736b1793bb017c3fcd0e31a918f5eb638e
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-02-13 17:31:58 -0700

    fix: Build

--- apps/flame-defi/app/earn/pages/vault-list/index.tsx
+++ apps/flame-defi/app/earn/pages/vault-list/index.tsx
@@ -1,6 +1,6 @@
 import { TableContextProvider } from "earn/pages/vault-list/contexts/table-context";
+import { HeaderSection } from "earn/pages/vault-list/sections/header-section";
 import { TableSection } from "earn/pages/vault-list/sections/table-section";
-import { HeaderSection } from "./sections/header-section";
 
 export const Page = () => {
   return (
