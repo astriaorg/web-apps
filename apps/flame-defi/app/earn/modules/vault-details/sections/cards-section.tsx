7c00ed45cc229384c29aa529c0596ef2b467fae0
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-02-19 20:52:12 -0700

    fix: Linting

--- apps/flame-defi/app/earn/modules/vault-details/sections/cards-section.tsx
+++ apps/flame-defi/app/earn/modules/vault-details/sections/cards-section.tsx
@@ -1,17 +1,12 @@
 import { APYChart } from "earn/modules/vault-details/components/apy-chart";
 import { SummaryCards } from "earn/modules/vault-details/components/summary-cards";
-import { usePageContext } from "earn/modules/vault-details/hooks/usePageContext";
 
 export const CardsSection = () => {
-  const {
-    query: { isPending, data },
-  } = usePageContext();
-
   return (
     <section className="flex flex-col px-4 md:px-20">
       <div className="mt-12">
         <SummaryCards />
-        <div className="mt-10">
+        <div className="mt-10 flex flex-col space-y-4">
           <span className="text-base/4 font-semibold">Overview</span>
           <APYChart />
         </div>
