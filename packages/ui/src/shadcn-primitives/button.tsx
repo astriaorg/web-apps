aca00d2fbe26eef6b45a3143c87ee0db2da60008
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-02-21 14:18:41 -0700

    feat: Update button component

--- packages/ui/src/shadcn-primitives/button.tsx
+++ packages/ui/src/shadcn-primitives/button.tsx
@@ -4,27 +4,31 @@ import * as React from "react";
 
 import { cn } from "../utils";
 
+const buttonStyles = {
+  default:
+    "bg-orange text-text-black shadow-[2px_2px_2px_0px_hsla(0,0%,100%,0.2)_inset]",
+  hover: `hover:bg-orange hover:text-text-black hover:shadow-[-2px_-2px_2px_0px_hsla(0,0%,0%,0.2)_inset,2px_2px_2px_0px_hsla(0,0%,100%,0.2)_inset]`,
+  active:
+    "active:bg-orange active:text-text-black active:shadow-[2px_2px_2px_0px_hsla(0,0%,0%,0.2)_inset]",
+};
+
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xl text-base font-semibold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
-        default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        default: `${buttonStyles.default} ${buttonStyles.hover} ${buttonStyles.active}`,
+        destructive: "",
+        outline: `border border-stroke ${buttonStyles.hover} ${buttonStyles.active}`,
+        secondary: "",
+        ghost: `${buttonStyles.hover} ${buttonStyles.active}`,
+        link: "",
       },
       size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
+        default: "h-10 px-4 py-3",
+        sm: "h-8 px-4 py-2 text-xs",
+        lg: "h-12 px-6 py-4",
+        icon: "h-10 w-10",
       },
     },
     defaultVariants: {
