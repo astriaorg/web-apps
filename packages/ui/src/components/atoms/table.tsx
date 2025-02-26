1b95500617650d32390aacde01086bd4b2aa9b8a
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-02-25 17:18:27 -0700

    fix: Table styles

--- packages/ui/src/components/atoms/table.tsx
+++ packages/ui/src/components/atoms/table.tsx
@@ -20,7 +20,7 @@ const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />
+  <thead ref={ref} className={cn("", className)} {...props} />
 ));
 TableHeader.displayName = "TableHeader";
 
@@ -28,11 +28,7 @@ const TableBody = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => (
-  <tbody
-    ref={ref}
-    className={cn("[&_tr:last-child]:border-0", className)}
-    {...props}
-  />
+  <tbody ref={ref} className={cn("", className)} {...props} />
 ));
 TableBody.displayName = "TableBody";
 
@@ -40,11 +36,7 @@ const TableFooter = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => (
-  <tfoot
-    ref={ref}
-    className={cn("border-t [&>tr]:last:border-b-0", className)}
-    {...props}
-  />
+  <tfoot ref={ref} className={cn("", className)} {...props} />
 ));
 TableFooter.displayName = "TableFooter";
 
@@ -52,11 +44,7 @@ const TableRow = React.forwardRef<
   HTMLTableRowElement,
   React.HTMLAttributes<HTMLTableRowElement>
 >(({ className, ...props }, ref) => (
-  <tr
-    ref={ref}
-    className={cn("border-b transition-colors", className)}
-    {...props}
-  />
+  <tr ref={ref} className={cn("transition-colors", className)} {...props} />
 ));
 TableRow.displayName = "TableRow";
 
