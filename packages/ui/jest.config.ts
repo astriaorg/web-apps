760ded0ae869a430a28e60ed56bf98fc1eaa5860
Jesse Snyder <jessetsnyder@gmail.com>
2025-02-28 16:41:41 -0700

    code causing lint was unneeded. fix deps. fix setup. ignore coverage dir

--- packages/ui/jest.config.ts
+++ packages/ui/jest.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   coverageProvider: "v8",
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.tsx"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 
   // The test environment that will be used for testing
   testEnvironment: "jsdom",
