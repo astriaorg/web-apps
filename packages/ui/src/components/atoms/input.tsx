02a89e2c698a9753cf68bab889cc31c3f65c1561
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-02-28 20:39:20 -0700

    fix: Remove unnecessary leading

--- packages/ui/src/components/atoms/input.tsx
+++ packages/ui/src/components/atoms/input.tsx
@@ -37,7 +37,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           type={type}
           className={cn(
-            "flex h-10 w-full rounded-xl px-4 py-2 text-sm/6 font-medium transition-colors placeholder:text-text-light outline-none",
+            "flex h-10 w-full rounded-xl px-4 py-2 text-sm font-medium transition-colors placeholder:text-text-light outline-none",
             "disabled:cursor-not-allowed disabled:opacity-50",
             "file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground",
             "focus-visible:outline-none",
