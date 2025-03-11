c4771de0409d33f27fd4e64414ed36b9ad0a73e3
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-03-10 20:07:52 -0600

    fix: Consistent headers, add back button

--- apps/flame-defi/app/earn/modules/market-details/sections/header-section.tsx
+++ apps/flame-defi/app/earn/modules/market-details/sections/header-section.tsx
@@ -1,16 +1,22 @@
 import { Badge, Skeleton } from "@repo/ui/components";
+import { BackButton } from "earn/components/back-button";
 import { MarketAssets } from "earn/components/market";
+import { ROUTES } from "earn/constants/routes";
 import { Asset, Maybe } from "earn/generated/gql/graphql";
 import { usePageContext } from "earn/modules/market-details/hooks/use-page-context";
+import { useRouter } from "next/navigation";
 
 export const HeaderSection = () => {
+  const router = useRouter();
   const {
     query: { data, isPending, status },
   } = usePageContext();
 
   return (
     <section className="flex flex-col px-4">
-      <div className="mt-18 flex flex-col">
+      <div className="relative mt-10 flex flex-col 2xl:flex-row 2xl:mt-18">
+        <BackButton onClick={() => router.push(ROUTES.MARKET_LIST)} />
+
         {status !== "error" && (
           <div className="flex flex-col space-y-3">
             <Skeleton isLoading={isPending}>
--- apps/flame-defi/app/earn/modules/market-list/sections/header-section.tsx
+++ apps/flame-defi/app/earn/modules/market-list/sections/header-section.tsx
@@ -1,9 +1,8 @@
 export const HeaderSection = () => {
   return (
-    <section className="flex flex-col px-4 md:px-20">
+    <section className="flex flex-col px-4 md:px-20 md:h-52">
       <div className="flex flex-col justify-between gap-4 mt-20 mb-6 md:flex-row md:mb-4">
-        <h1 className="text-xl/6">Borrow</h1>
-        {/* <MarketSummary /> */}
+        <h1 className="text-3xl/8 font-medium">Borrow</h1>
       </div>
     </section>
   );
