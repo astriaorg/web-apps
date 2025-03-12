b6c36876c07dccc02f1b75460af78533d8e3ec4d
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-03-11 21:59:29 -0600

    feat: Add generic chart utils and tests

---
+++ packages/ui/src/components/organisms/chart/chart.utils.test.ts
@@ -0,0 +1,91 @@
+import { CHART_TICK_INTERVALS } from "./chart.types";
+import { getDownsampledData, getTickIntervalData } from "./chart.utils";
+
+describe("getDownsampledData", () => {
+  it("should return an empty array when input data is empty", () => {
+    const data: number[] = [];
+    const result = getDownsampledData(data, { maximumDataPoints: 10 });
+    expect(result).toEqual([]);
+  });
+
+  it("should return the same data if the data length is less than or equal to the target length", () => {
+    const data = [1, 2, 3, 4, 5];
+    const result = getDownsampledData(data, { maximumDataPoints: 5 });
+    expect(result).toEqual(data);
+  });
+
+  it("should downsample the data to the target length", () => {
+    const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const result = getDownsampledData(data, { maximumDataPoints: 5 });
+    expect(result.length).toBe(5);
+  });
+
+  it("should handle complex objects correctly", () => {
+    const data = [{ x: 1 }, { x: 2 }, { x: 3 }, { x: 4 }, { x: 5 }];
+    const result = getDownsampledData(data, { maximumDataPoints: 1 });
+    expect(result.length).toBe(1);
+  });
+});
+
+describe("getTickIntervalData", () => {
+  it("should return an empty array when input data is empty", () => {
+    const data: number[] = [];
+    for (const interval of CHART_TICK_INTERVALS) {
+      const result = getTickIntervalData(data, { interval, key: "" as any });
+      expect(result).toEqual([]);
+    }
+  });
+
+  it("should return the same data if every interval is unique", () => {
+    const data = [
+      { x: new Date("2024-01-01").getTime(), y: 0 },
+      { x: new Date("2024-02-01").getTime(), y: 0 },
+      { x: new Date("2024-03-01").getTime(), y: 0 },
+      { x: new Date("2024-04-01").getTime(), y: 0 },
+      { x: new Date("2024-05-01").getTime(), y: 0 },
+      { x: new Date("2024-06-01").getTime(), y: 0 },
+    ];
+    for (const interval of CHART_TICK_INTERVALS) {
+      const result = getTickIntervalData(data, { interval, key: "x" });
+      expect(result).toEqual(data);
+    }
+  });
+
+  it("1w interval should work correctly", () => {
+    const data = [
+      { x: new Date("2024-01-01").getTime(), y: 0 },
+      { x: new Date("2024-01-03").getTime(), y: 0 }, // Same week.
+      { x: new Date("2024-01-08").getTime(), y: 0 }, // + 1 week.
+      { x: new Date("2024-01-09").getTime(), y: 0 }, // + 1 week, same week.
+      { x: new Date("2024-01-15").getTime(), y: 0 }, // + 2 weeks.
+      { x: new Date("2024-02-01").getTime(), y: 0 },
+      { x: new Date("2024-03-01").getTime(), y: 0 },
+    ];
+    const result = getTickIntervalData(data, { interval: "1w", key: "x" });
+    expect(result).toEqual([
+      { x: new Date("2024-01-01").getTime(), y: 0 },
+      { x: new Date("2024-01-08").getTime(), y: 0 },
+      { x: new Date("2024-01-15").getTime(), y: 0 },
+      { x: new Date("2024-02-01").getTime(), y: 0 },
+      { x: new Date("2024-03-01").getTime(), y: 0 },
+    ]);
+  });
+
+  it("1m interval should work correctly", () => {
+    const data = [
+      { x: new Date("2023-12-01").getTime(), y: 0 },
+      { x: new Date("2023-12-31").getTime(), y: 0 }, // Same month.
+      { x: new Date("2024-01-01").getTime(), y: 0 }, // + 1 month.
+      { x: new Date("2024-01-31").getTime(), y: 0 }, // + 1 month, same month.
+      { x: new Date("2024-02-01").getTime(), y: 0 }, // + 2 months.
+      { x: new Date("2024-03-01").getTime(), y: 0 },
+    ];
+    const result = getTickIntervalData(data, { interval: "1m", key: "x" });
+    expect(result).toEqual([
+      { x: new Date("2023-12-01").getTime(), y: 0 },
+      { x: new Date("2024-01-01").getTime(), y: 0 },
+      { x: new Date("2024-02-01").getTime(), y: 0 },
+      { x: new Date("2024-03-01").getTime(), y: 0 },
+    ]);
+  });
+});
--- packages/ui/src/components/organisms/chart/chart.utils.ts
+++ packages/ui/src/components/organisms/chart/chart.utils.ts
@@ -1,4 +1,6 @@
-import { ChartConfig } from "./chart.types";
+import { ChartConfig, ChartTickInterval } from "./chart.types";
+
+type DateParams = number | string | Date;
 
 // Helper to extract item config from a payload.
 export function getPayloadConfigFromPayload(
@@ -38,3 +40,112 @@ export function getPayloadConfigFromPayload(
     ? config[configLabelKey]
     : config[key as keyof typeof config];
 }
+
+/**
+ * Downsamples data to a maximum number of data points.
+ * Useful for displaying large datasets in charts to avoid performance issues.
+ */
+export const getDownsampledData = <T>(
+  data: T[],
+  options: {
+    /**
+     * Threshold for the maximum number of data points to display.
+     */
+    maximumDataPoints: number;
+  } = { maximumDataPoints: 1000 },
+) => {
+  if (data.length > options.maximumDataPoints) {
+    const downsampled: T[] = [];
+    const interval = Math.ceil(data.length / options.maximumDataPoints);
+
+    for (let i = 0; i < data.length; i += interval) {
+      downsampled.push(data[i] as T);
+    }
+
+    return downsampled;
+  }
+
+  return data;
+};
+
+/**
+ *  @returns An array of data points representing the first data point of each interval.
+ */
+export const getTickIntervalData = <T>(
+  data: T[],
+  options: {
+    interval: ChartTickInterval;
+    /**
+     * The accessor key for the date value in the data point.
+     */
+    key: keyof T;
+  },
+): (T | null)[] => {
+  const result: (T | null)[] = [];
+
+  if (!data.length) {
+    return result;
+  }
+
+  switch (options.interval) {
+    case "3d":
+    case "1w": {
+      const milliseconds = (() => {
+        switch (options.interval) {
+          case "3d":
+            return 1000 * 60 * 60 * 24 * 3;
+          case "1w":
+            return 1000 * 60 * 60 * 24 * 7;
+          default:
+            throw new Error(
+              `Invalid chart tick interval: "${options.interval}".`,
+            );
+        }
+      })();
+
+      let previousDate = data[0]
+        ? new Date(data[0][options.key as keyof T] as DateParams).getTime()
+        : 0;
+      result.push(data[0] as T);
+
+      for (let i = 1; i < data.length; i++) {
+        const currentDate = new Date(
+          (data[i] as T)[options.key as keyof T] as DateParams,
+        ).getTime();
+        if (currentDate >= previousDate + milliseconds) {
+          result.push(data[i] as T);
+          previousDate = currentDate;
+        }
+      }
+
+      break;
+    }
+    case "1m": {
+      let previousItem: T = data[0] as T;
+      result.push(data[0] as T);
+
+      for (let i = 1; i < data.length; i++) {
+        const currentMonth = new Date(
+          (data[i] as T)[options.key as keyof T] as DateParams,
+        ).getUTCMonth();
+        const previousMonth = previousItem
+          ? new Date(
+              previousItem[options.key as keyof T] as DateParams,
+            ).getUTCMonth()
+          : -1;
+
+        if (currentMonth !== previousMonth) {
+          result.push(data[i] as T);
+          previousItem = data[i] as T;
+        }
+      }
+
+      break;
+    }
+    default: {
+      throw new Error(`Invalid chart tick interval: "${options.interval}".`);
+    }
+  }
+
+  return result;
+};
