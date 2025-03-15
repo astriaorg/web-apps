ea9bdda164a4e7770c29a3dfbe5e23fb0108e005
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-03-14 21:25:58 -0600

    feat: Add 'copied' text on copy

--- packages/ui/src/components/molecules/copy-to-clipboard/copy-to-clipboard.tsx
+++ packages/ui/src/components/molecules/copy-to-clipboard/copy-to-clipboard.tsx
@@ -21,31 +21,58 @@ export const CopyToClipboard = ({
   ...props
 }: CopyToClipboardProps) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [hasCopied, setHasCopied] = useState(false);
 
   const onCopy = useCallback(() => {
-    console.log("Copying to clipboard:", value);
     navigator.clipboard.writeText(value);
   }, [value]);
 
   // TODO: Call existing events, if any, before we overwrite.
+  // TODO: Fix janky animations on hovering over the border of the trigger.
   const children = useMemo(() => {
     return React.cloneElement(props.children as React.ReactElement, {
-      onClick: onCopy,
-      onMouseEnter: () => setIsOpen(true),
+      onClick: () => {
+        onCopy();
+        setHasCopied(true);
+        setIsOpen(true);
+
+        setTimeout(() => setIsOpen(false), 2500);
+      },
+      onMouseEnter: () => {
+        setHasCopied(false);
+        setIsOpen(true);
+      },
       onMouseLeave: () => setIsOpen(false),
     });
   }, [onCopy, props.children]);
+
+  const tooltipContent = useMemo(() => {
+    if (hasCopied) {
+      return <span className="text-success">Copied!</span>;
+    }
+    return content;
+  }, [hasCopied, content]);
 
   if (!content) {
     return children;
   }
 
   return (
-    <TooltipProvider>
-      <Tooltip open={isOpen} onOpenChange={setIsOpen}>
-        <TooltipTrigger asChild>{children}</TooltipTrigger>
-        <TooltipContent side={side || "top"}>{content}</TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <div
+      // Fixes tooltip automatically opening when page is focused.
+      // Ref: https://github.com/radix-ui/primitives/issues/2248
+      onFocusCapture={(e) => {
+        e.stopPropagation();
+      }}
+    >
+      <TooltipProvider>
+        <Tooltip open={isOpen} onOpenChange={setIsOpen}>
+          <TooltipTrigger asChild onClick={() => setIsOpen(true)}>
+            {children}
+          </TooltipTrigger>
+          <TooltipContent side={side || "top"}>{tooltipContent}</TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
   );
 };
