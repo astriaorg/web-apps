0fa8f433912a4ed6f5d21a7efde8e34da59873dc
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-04-17 04:03:01 -0600

    feat: Add `calculateTickToPrice`

--- apps/flame-defi/app/pool/utils/pool-helpers.test.ts
+++ apps/flame-defi/app/pool/utils/pool-helpers.test.ts
@@ -1,22 +1,25 @@
 import Big from "big.js";
-import { getPoolExchangeRate } from "./pool-helpers";
+import {
+  calculatePoolExchangeRate,
+  calculateTickToPrice,
+} from "./pool-helpers";
+
+const TOKEN_0 = {
+  coinDenom: "USDC",
+  coinDecimals: 6,
+};
+
+const TOKEN_1 = {
+  coinDenom: "WETH",
+  coinDecimals: 18,
+};
 
 // Test our implementation against the values in the Uniswap V3 documentation example.
-describe("getPoolExchangeRate", () => {
+describe("calculatePoolExchangeRate", () => {
   it("should return correct exchange rate", () => {
-    const token0 = {
-      coinDenom: "USDC",
-      coinDecimals: 6,
-    } as any; // eslint-disable-line @typescript-eslint/no-explicit-any
-
-    const token1 = {
-      coinDenom: "WETH",
-      coinDecimals: 18,
-    } as any; // eslint-disable-line @typescript-eslint/no-explicit-any
-
-    const result = getPoolExchangeRate({
-      token0,
-      token1,
+    const result = calculatePoolExchangeRate({
+      decimal0: TOKEN_0.coinDecimals,
+      decimal1: TOKEN_1.coinDecimals,
       slot0: {
         sqrtPriceX96: 2018382873588440326581633304624437n,
         tick: 0,
@@ -29,10 +32,23 @@ describe("getPoolExchangeRate", () => {
     });
 
     expect(
-      new Big(result.rateToken0ToToken1).toFixed(token1.coinDecimals),
+      new Big(result.rateToken0ToToken1).toFixed(TOKEN_1.coinDecimals),
     ).toEqual("0.000649004842701370"); // 1 USDC = _ WETH
     expect(
-      new Big(result.rateToken1ToToken0).toFixed(token0.coinDecimals),
+      new Big(result.rateToken1ToToken0).toFixed(TOKEN_0.coinDecimals),
     ).toEqual("1540.820552"); // 1 WETH = _ USDC
+  });
+});
+
+describe("calculateTickToPrice", () => {
+  it("should return correct price", () => {
+    const result = calculateTickToPrice({
+      decimal0: TOKEN_0.coinDecimals,
+      decimal1: TOKEN_1.coinDecimals,
+      tick: 267967,
+    });
+
+    expect(result.priceToken0ToToken1).toEqual(0.43358784516599413);
+    expect(result.priceToken1ToToken0).toEqual(2.306337714834145);
   });
 });
--- apps/flame-defi/app/pool/utils/pool-helpers.ts
+++ apps/flame-defi/app/pool/utils/pool-helpers.ts
@@ -1,4 +1,3 @@
-import type { EvmCurrency } from "@repo/flame-types";
 import Big from "big.js";
 import type { Slot0Data } from "features/evm-wallet";
 
@@ -9,13 +8,13 @@ import type { Slot0Data } from "features/evm-wallet";
  *
  * Ref: https://blog.uniswap.org/uniswap-v3-math-primer#how-do-i-calculate-the-current-exchange-rate
  */
-export const getPoolExchangeRate = ({
-  token0,
-  token1,
+export const calculatePoolExchangeRate = ({
+  decimal0,
+  decimal1,
   slot0,
 }: {
-  token0: EvmCurrency;
-  token1: EvmCurrency;
+  decimal0: number;
+  decimal1: number;
   slot0: Slot0Data;
 }): {
   /**
@@ -28,19 +27,44 @@ export const getPoolExchangeRate = ({
   rateToken1ToToken0: string;
 } => {
   const sqrtPriceX96 = new Big(slot0.sqrtPriceX96.toString());
-  const decimal0 = token0.coinDecimals;
-  const decimal1 = token1.coinDecimals;
 
   // Calculate the numerator: (sqrtPriceX96 / 2**96)**2
-  const numerator = sqrtPriceX96.div(Big(2).pow(96)).pow(2);
+  const numerator = sqrtPriceX96.div(new Big(2).pow(96)).pow(2);
   // Calculate the denominator: 10**Decimal1 / 10**Decimal0
-  const denominator = Big(10).pow(decimal1).div(Big(10).pow(decimal0));
+  const denominator = new Big(10 ** Math.abs(decimal1 - decimal0));
 
-  const buyOneOfToken0 = numerator.div(denominator).toFixed();
-  const buyOneOfToken1 = Big(1).div(buyOneOfToken0).toFixed();
+  const buyOneOfToken0 = new Big(numerator).div(denominator).toFixed();
+  const buyOneOfToken1 = new Big(1).div(buyOneOfToken0).toFixed();
 
   return {
     rateToken0ToToken1: buyOneOfToken0,
     rateToken1ToToken0: buyOneOfToken1,
+  };
+};
+
+/**
+ * Calculates the price of token0 in terms of token1 and vice versa using the tick value.
+ */
+export const calculateTickToPrice = ({
+  tick,
+  decimal0,
+  decimal1,
+}: {
+  tick: number;
+  decimal0: number;
+  decimal1: number;
+}): {
+  priceToken0ToToken1: number;
+  priceToken1ToToken0: number;
+} => {
+  // Calculate price0: (1.0001 ** tick) / (10 ** (Decimal1 - Decimal0))
+  const price0 = Math.pow(1.0001, tick) / Math.pow(10, decimal1 - decimal0);
+
+  // Calculate price1: 1 / price0
+  const price1 = 1 / price0;
+
+  return {
+    priceToken0ToToken1: price0,
+    priceToken1ToToken0: price1,
   };
 };
