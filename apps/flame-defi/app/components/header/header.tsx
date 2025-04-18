e254495340d628b1811af3ec5c8ae84683122e2c
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-04-17 23:49:05 -0600

    feat: Add header component

---
+++ apps/flame-defi/app/components/header/header.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@repo/ui/components";
+import { ArrowLeftIcon } from "@repo/ui/icons";
+import type { PropsWithChildren } from "react";
+
+interface HeaderProps extends PropsWithChildren {
+  children: React.ReactNode;
+  onClickBack?: () => void;
+}
+
+// TODO: Apply this component to earn pages.
+export const Header = ({ children, onClickBack }: HeaderProps) => {
+  return (
+    <section className="flex flex-col mb-8">
+      <div className="flex flex-col lg:flex-row">
+        <div className="relative flex flex-col lg:flex-row">
+          {onClickBack && (
+            <Button
+              className="h-8 w-8 text-icon-light mb-4 -ml-2 lg:absolute lg:-left-12 lg:mb-0 lg:ml-0"
+              variant="ghost"
+              onClick={onClickBack}
+            >
+              <ArrowLeftIcon aria-label="Back" size={16} />
+            </Button>
+          )}
+          {children}
+        </div>
+      </div>
+    </section>
+  );
+};
--- apps/flame-defi/app/pool/modules/create-position/index.tsx
+++ apps/flame-defi/app/pool/modules/create-position/index.tsx
@@ -1,9 +1,11 @@
 import { PageContextProvider } from "pool/modules/create-position/contexts/page-context";
 import { ContentSection } from "pool/modules/create-position/sections/content-section";
+import { HeaderSection } from "pool/modules/create-position/sections/header-section";
 
 export const Page = () => {
   return (
     <PageContextProvider>
+      <HeaderSection />
       <ContentSection />
     </PageContextProvider>
   );
