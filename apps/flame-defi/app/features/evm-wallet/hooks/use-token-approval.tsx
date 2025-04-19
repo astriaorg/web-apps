d1e855913040ce784f618d52201e25ab3036d879
Jesse Snyder <jessetsnyder@gmail.com>
2025-04-19 01:57:01 -0600

    remove unused dep

--- apps/flame-defi/app/features/evm-wallet/hooks/use-token-approval.tsx
+++ apps/flame-defi/app/features/evm-wallet/hooks/use-token-approval.tsx
@@ -129,7 +129,6 @@ export const useTokenApproval = ({
     setTokenAllowances(newTokenAllowances);
   }, [
     userAccount.address,
-    userAccount.chainId,
     currencies,
     wagmiConfig,
     chain.chainId,
