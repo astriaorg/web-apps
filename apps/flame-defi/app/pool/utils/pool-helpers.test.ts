adf45c97fa58e68a739e7ba9a20e830cfb0100d5
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-04-24 18:16:44 -0600

    fix: Move variables into test

--- apps/flame-defi/app/pool/utils/pool-helpers.test.ts
+++ apps/flame-defi/app/pool/utils/pool-helpers.test.ts
@@ -1,30 +1,30 @@
 import Big from "big.js";
 import { calculatePoolExchangeRate } from "./pool-helpers";
 
-const TOKEN_0 = {
-  coinDenom: "USDC",
-  coinDecimals: 6,
-};
-
-const TOKEN_1 = {
-  coinDenom: "WETH",
-  coinDecimals: 18,
-};
-
 // Test our implementation against the values in the Uniswap V3 documentation example.
 describe("calculatePoolExchangeRate", () => {
+  const token0 = {
+    coinDenom: "USDC",
+    coinDecimals: 6,
+  };
+
+  const token1 = {
+    coinDenom: "WETH",
+    coinDecimals: 18,
+  };
+
   it("should return correct exchange rate", () => {
     const result = calculatePoolExchangeRate({
-      decimal0: TOKEN_0.coinDecimals,
-      decimal1: TOKEN_1.coinDecimals,
+      decimal0: token0.coinDecimals,
+      decimal1: token1.coinDecimals,
       sqrtPriceX96: 2018382873588440326581633304624437n,
     });
 
     expect(
-      new Big(result.rateToken0ToToken1).toFixed(TOKEN_1.coinDecimals),
+      new Big(result.rateToken0ToToken1).toFixed(token1.coinDecimals),
     ).toEqual("0.000649004842701370"); // 1 USDC = _ WETH
     expect(
-      new Big(result.rateToken1ToToken0).toFixed(TOKEN_0.coinDecimals),
+      new Big(result.rateToken1ToToken0).toFixed(token0.coinDecimals),
     ).toEqual("1540.820552"); // 1 WETH = _ USDC
   });
 
