70f143231913217c14ac321c18b3d88ad4c84c8c
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-04-24 18:46:33 -0600

    test: Add `calculateTickToPrice` test

--- apps/flame-defi/app/pool/utils/pool-helpers.test.ts
+++ apps/flame-defi/app/pool/utils/pool-helpers.test.ts
@@ -1,13 +1,16 @@
 import Big from "big.js";
-import { calculatePoolExchangeRate } from "./pool-helpers";
+import {
+  calculatePoolExchangeRate,
+  calculateTickToPrice,
+} from "./pool-helpers";
 
 // Test our implementation against the values in the Uniswap V3 documentation example.
 describe("calculatePoolExchangeRate", () => {
+  // price = token1/token0 = WETH/USDC
   const token0 = {
     coinDenom: "USDC",
     coinDecimals: 6,
   };
-
   const token1 = {
     coinDenom: "WETH",
     coinDecimals: 18,
@@ -37,5 +40,47 @@ describe("calculatePoolExchangeRate", () => {
 
     expect(result.rateToken0ToToken1).toEqual("0.36535748767549793233");
     expect(result.rateToken1ToToken0).toEqual("2.73704531515767610072");
+  });
+});
+
+describe("calculatePriceToTick and calculateTickToPrice", () => {
+  // USDC/WETH
+  const token0 = {
+    coinDenom: "WETH",
+    coinDecimals: 18,
+  };
+  const token1 = {
+    coinDenom: "USDC",
+    coinDecimals: 6,
+  };
+
+  it("calculateTickToPrice", () => {
+    const tickLower = 202910;
+    const tickUpper = 202920;
+
+    const priceLower = calculateTickToPrice({ tick: tickLower });
+    const priceUpper = calculateTickToPrice({ tick: tickUpper });
+
+    expect(priceLower).toEqual(648378713.2500573);
+    expect(priceUpper).toEqual(649027383.8115474);
+
+    const decimalAdjustedPriceLower = calculateTickToPrice({
+      tick: tickLower,
+      decimal0: token0.coinDecimals,
+      decimal1: token1.coinDecimals,
+    });
+    const decimalAdjustedPriceUpper = calculateTickToPrice({
+      tick: tickUpper,
+      decimal0: token0.coinDecimals,
+      decimal1: token1.coinDecimals,
+    });
+
+    // Example has values truncated to 2 decimal places.
+    expect(new Big(1 / decimalAdjustedPriceLower).toFixed(2, 0)).toEqual(
+      "1542.30",
+    );
+    expect(new Big(1 / decimalAdjustedPriceUpper).toFixed(2, 0)).toEqual(
+      "1540.76",
+    );
   });
 });
