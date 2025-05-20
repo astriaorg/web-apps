9cde36af22f37c2d3c2de150ce1077a12f463435
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-05-20 02:15:33 -0600

    feat: Add collect fees logic V2

--- apps/flame-defi/app/pool/hooks/index.ts
+++ apps/flame-defi/app/pool/hooks/index.ts
@@ -1,6 +1,5 @@
 export * from "./use-add-liquidity-transaction";
 export * from "./use-add-liquidity-validation";
-export * from "./use-collect-fees-transaction";
 export * from "./use-pool-context";
 export * from "./use-pool-position-context";
 export * from "./use-remove-liquidity-percentage";
---
+++ apps/flame-defi/app/pool/hooks/use-collect-fees.tsx
@@ -0,0 +1,65 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useCallback } from "react";
+import { useAccount, useConfig } from "wagmi";
+
+import { useAstriaChainData } from "config";
+import {
+  type CollectFeesV2Params,
+  createNonfungiblePositionManagerService,
+} from "features/evm-wallet";
+
+import { usePoolPositionContext } from ".";
+
+export const useCollectFees = () => {
+  const queryClient = useQueryClient();
+  const { tokenId } = usePoolPositionContext();
+  const { address } = useAccount();
+  const config = useConfig();
+  const { chain } = useAstriaChainData();
+
+  const mutation = useMutation({
+    mutationFn: async (params: CollectFeesV2Params) => {
+      if (!address || !tokenId || !tokenId) {
+        throw new Error("Missing required data for collecting fees.");
+      }
+
+      const nonfungiblePositionService =
+        createNonfungiblePositionManagerService(
+          config,
+          chain.contracts.nonfungiblePositionManager.address,
+        );
+
+      const result = await nonfungiblePositionService.collectFeesV2(params);
+
+      return result;
+    },
+    onSuccess: (hash) => {
+      if (hash) {
+        void queryClient.invalidateQueries({
+          queryKey: ["useGetPosition", chain.chainId, tokenId],
+        });
+      }
+    },
+    onError: (error) => {
+      console.warn("Error collecting fees:", error);
+    },
+  });
+
+  const collectFees = useCallback(
+    async (params: CollectFeesV2Params) => {
+      try {
+        const result = await mutation.mutateAsync(params);
+        return result;
+      } catch (error) {
+        console.error("Error in collect fees:", error);
+        throw error;
+      }
+    },
+    [mutation],
+  );
+
+  return {
+    collectFees,
+    ...mutation,
+  };
+};
