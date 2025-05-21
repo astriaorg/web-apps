fcefd58a29db627c2242c1fb420c6c9caeeea459
Jesse Snyder <jessetsnyder@gmail.com>
2025-05-20 18:03:07 -0600

    remove cruft

--- apps/flame-defi/app/components/connect-wallet/index.ts
+++ apps/flame-defi/app/components/connect-wallet/index.ts
@@ -1,3 +1,2 @@
 export { ConnectMultipleWallets } from "./connect-multiple-wallets";
 export { ConnectWalletContent } from "./connect-wallet-content";
-export { WalletInterstitial } from "./wallet-interstitial";
