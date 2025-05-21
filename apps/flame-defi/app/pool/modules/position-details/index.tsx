070eecd792fbebdc8de53b57613db3428ab89b4e
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-05-20 21:29:42 -0600

    fix: Remove unused context

--- apps/flame-defi/app/pool/modules/position-details/index.tsx
+++ apps/flame-defi/app/pool/modules/position-details/index.tsx
@@ -1,4 +1,3 @@
-import { PoolPositionContextProvider } from "pool/context/pool-position-context";
 import { PoolPositionContextProvider as PoolPositionContextProviderV2 } from "pool/context/pool-position-context-v2";
 
 import { ContentSection } from "./sections/content-section";
@@ -6,11 +5,9 @@ import { HeaderSection } from "./sections/header-section";
 
 export const Page = () => {
   return (
-    <PoolPositionContextProvider>
-      <PoolPositionContextProviderV2>
-        <HeaderSection />
-        <ContentSection />
-      </PoolPositionContextProviderV2>
-    </PoolPositionContextProvider>
+    <PoolPositionContextProviderV2>
+      <HeaderSection />
+      <ContentSection />
+    </PoolPositionContextProviderV2>
   );
 };
