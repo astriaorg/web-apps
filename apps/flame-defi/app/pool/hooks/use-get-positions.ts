4837c705a1f2b33afa3bcda1d47ce41d26dfd5d5
Jessica Xiao <15721063+essj@users.noreply.github.com>
2025-05-28 03:08:21 -0600

    fix: Use position liquidity, not pool liquidity

--- apps/flame-defi/app/pool/hooks/use-get-positions.ts
+++ apps/flame-defi/app/pool/hooks/use-get-positions.ts
@@ -97,7 +97,6 @@ export const useGetPositions = (): UseQueryResult<
         return {
           ...position,
           address: uniquePool,
-          liquidity: uniqueLiquidityAndSlot0.liquidity,
           slot0: uniqueLiquidityAndSlot0.slot0,
         };
       });
